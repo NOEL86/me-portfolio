6e583d425d6a83dc3970b8b9b29e8e15cf06dade
Ashley <anoelj86@yahoo.com>
2018-09-22 22:47:38 -0600

    node issues

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+var routes = require("./routes");
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("App now listening at localhost:" + PORT);
+});
