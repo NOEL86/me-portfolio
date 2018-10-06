d0b2a6abfbb05e15d198c4e4caba7383ab37ab17
Ashley <anoelj86@yahoo.com>
2018-10-06 12:19:40 -0600

    front end errors

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 var PORT = process.env.PORT || 3000;
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 // parse application/json
 app.use(bodyParser.json());
