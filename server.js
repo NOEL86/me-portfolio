cd6a2460e471587bfa5e1f70b93ca7159fd9bcb7
Ashley <anoelj86@yahoo.com>
2018-10-21 19:35:49 -0600

    fixed css

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
+
 // parse application/json
 app.use(bodyParser.json());
 
 var routes = require("./routes");
 app.use(routes);
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
     console.log("App now listening at localhost:" + PORT);
 });
